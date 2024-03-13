284eafb0007aa823dac2209ac56bee7b83e4b476
Abir Chodha <abirchodha1@gmail.com>
2024-03-13 20:41:27 +0530

    Implement transfers

--- script.js
+++ script.js
@@ -147,3 +147,22 @@ btnLogin.addEventListener('click', function (e) {
     calcDisplaySummary(currentAccount);
   }
 });
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+  }
+});
